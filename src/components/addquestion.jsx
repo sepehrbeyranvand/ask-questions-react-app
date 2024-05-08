f8d2265d75e4f9f59d5e4d555a800677f6399ea5
sepehrbeyranvand <sepehr7797@gmail.com>
2024-05-08 10:33:33 +0330

    add questions to the direbase database

--- src/components/addquestion.jsx
+++ src/components/addquestion.jsx
@@ -1,11 +1,13 @@
+import { addDoc, collection } from "firebase/firestore";
 import { useState } from "react";
+import { db } from "../firebase/config";
+import Swal from "sweetalert2";
 
 export default function Addquestion() {
   //VARIABLES
   const [title, setTitle] = useState("");
   const [tags, setTags] = useState("");
   const [describe, setDescribe] = useState("");
-  const [question, setQuestion] = useState([]);
 
   //FUNCTIONS
   const resetAll = () => {
@@ -13,24 +15,29 @@ export default function Addquestion() {
     setTags("");
     setDescribe("");
   };
-  const submitForm = (e) => {
+
+  const sendForm = async (e) => {
     e.preventDefault();
-  };
-  const sendForm = () => {
-    const objCards = {
-      title: title,
-      tags: tags,
-      describe: describe,
-    };
-    setQuestion((prevQuestion) => {
-      return [...prevQuestion, objCards];
-    });
-    setTags(tags.split(","));
+    const doc = { title, tags, describe };
+    try {
+      const ref = collection(db, "first");
+      await addDoc(ref, doc);
+      Swal.fire({
+        position: "top-end",
+        icon: "success",
+        title: "Your question added to the question box",
+        showConfirmButton: false,
+        timer: 2000,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+    resetAll();
   };
   return (
     <div>
       <form
-        onSubmit={submitForm}
+        onSubmit={sendForm}
         className="flex justify-center items-center flex-col"
       >
         <div className="flex justify-center items-center mt-16 w-full">
@@ -41,6 +48,7 @@ export default function Addquestion() {
               type="text"
               value={title}
               onChange={(e) => setTitle(e.target.value)}
+              required
             />
           </div>
           <div className="flex flex-col mx-12">
@@ -50,6 +58,7 @@ export default function Addquestion() {
               type="text"
               value={tags}
               onChange={(e) => setTags(e.target.value)}
+              required
             />
           </div>
         </div>
@@ -60,30 +69,23 @@ export default function Addquestion() {
             rows="7"
             value={describe}
             onChange={(e) => setDescribe(e.target.value)}
+            required
           ></textarea>
         </div>
         <div className="flex justify-between items-center w-[50vw] mt-10">
           <button
             onClick={resetAll}
+            type="button"
             className="bg-red-600 rounded-lg text-white p-2 w-40 hover:bg-red-700 transition .4s ease-in-out duration-200"
           >
             Reset
           </button>
-          <button
-            onClick={sendForm}
-            className="bg-green-600 rounded-lg text-white p-2 w-40 hover:bg-green-700 transition .4s ease-in-out duration-200"
-          >
+          <button className="bg-green-600 rounded-lg text-white p-2 w-40 hover:bg-green-700 transition .4s ease-in-out duration-200">
             Submit
           </button>
         </div>
       </form>
-      <div>
-        {question.map((e, index) => (
-          <p key={index}>
-            {e.title}-{e.tags}-{e.describe}
-          </p>
-        ))}
-      </div>
+      <div></div>
     </div>
   );
 }
